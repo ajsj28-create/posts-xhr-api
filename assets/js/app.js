fbb833d456ad44c9666126771cc05aaabd9ecb6a
ajsj.28 <ajsj.28@gmail.com>
2025-09-03 22:21:24 +0530

    version 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,64 +4,92 @@ const postsForm = document.getElementById('postsForm');
 const userIdControl = document.getElementById('userId');
 const titleControl = document.getElementById('title');
 const bodyControl = document.getElementById('body');
-const display = document.getElementById('display');
+const postsContainer = document.getElementById('postsContainer');
 const addBtn = document.getElementById('addBtn');
 const updateBtn = document.getElementById('updateBtn');
+const loader = document.getElementById('loader');
 
 let base_url = `https://jsonplaceholder.typicode.com`;
 let postsData_url = `${base_url}/posts`;
+
+const showLoader = () => {
+    loader.classList.remove('d-none')
+};
+
+const hideLoader = () => {
+    loader.classList.add('d-none')
+};
+
+const snackBar = (msg, icon) => {
+    hideLoader()
+    swal.fire({
+        title: msg,
+        icon: icon,
+        // timer: 2500
+    })
+};
+
+const makeAPIcall = (method, url, body) => {
+    let xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.setRequestHeader('Authorization', 'JWT from LS')
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.send(JSON.stringify(body))
+    return xhr
+};
 
 const templating = (arr) => {
     let result = ``
     arr.forEach(obj => {
         result += `
-            <div class="col-md-4 mb-3" id="${obj.id}">
-                <div class="card h-100">
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3" id="${obj.id}">
+                <div class="card h-100 postCard">
                     <div class="card-header postHead color-dark">
                         <h4 class="m-0">${obj.title}</h4>
                     </div>
                     <div class="card-body postBody color-light">
                         <p class="m-0">${obj.body}</p>
                     </div>
-                    <div class="card-footer color-dark d-flex justify-content-between align-items-center">
+                    <div class="card-footer postFoot color-dark d-flex justify-content-between align-items-center">
                         <div class="button-group">
-							<button onclick="onEdit(this)" class="btn btn-primary mr-2">Edit</button>
+							<button onclick="onEdit(this)" class="btn btn-primary m-2">Edit</button>
                             <button onclick="onRemove(this)" class="btn btn-danger">Remove</button>
 						</div>
-						<span>by User ${obj.userId}</span>
+						<span>User ${obj.userId}</span>
                     </div>
                 </div>
             </div>`
-        
-        display.innerHTML = result;    
+        postsContainer.innerHTML = result;   
     })
+    hideLoader() 
 };
 
 const showNewPost = (obj) => {
     let newCol = document.createElement('div')
     newCol.innerHTML = `
-            <div class="card h-100">
+            <div class="card h-100 postCard">
                     <div class="card-header postHead color-dark">
                         <h4 class="m-0">${obj.title}</h4>
                     </div>
                     <div class="card-body postBody color-light">
                         <p class="m-0">${obj.body}</p>
                     </div>
-                    <div class="card-footer color-dark d-flex justify-content-between align-items-center">
+                    <div class="card-footer postFoot color-dark d-flex justify-content-between align-items-center">
                         <div class="button-group">
-							<button onclick="onEdit(this)" class="btn btn-primary mr-2">Edit</button>
+							<button onclick="onEdit(this)" class="btn btn-primary m-2">Edit</button>
                             <button onclick="onRemove(this)" class="btn btn-danger">Remove</button>
 						</div>
-						<span>by User ${obj.userId}</span>
+						<span>User ${obj.userId}</span>
                     </div>
             </div>`
-    newCol.className = `col-md-4 mb-3`
+    newCol.className = `col-lg-4 col-md-6 col-sm-12 mb-3`
 
     newCol.id = localStorage.getItem('newId')
     localStorage.removeItem('newId')
-    
-    display.prepend(newCol)
+
+    postsContainer.prepend(newCol)
     postsForm.reset()
+    snackBar(`New post added successfully!!!`, `success`)
 };
 
 const patch = (obj) => {
@@ -71,29 +99,46 @@ const patch = (obj) => {
 
     addBtn.classList.add('d-none')
     updateBtn.classList.remove('d-none')
+
+    hideLoader()
 };
 
-const fetchPosts = () => { // GET method
+const showUpdatedPost = (obj) => {
+    let children = document.getElementById(obj.id).firstElementChild.children
+    children[0].innerHTML = `<h4 class="m-0">${obj.title}</h4>`
+    children[1].innerHTML = `<p class="m-0">${obj.body}</p>`
+    children[2].lastElementChild.innerHTML = `User ${obj.userId}`
 
-    let xhr = new XMLHttpRequest()
+    postsForm.reset()
+    addBtn.classList.remove('d-none')
+    updateBtn.classList.add('d-none')
+    snackBar(`Post updated Successfully!!!`, `success`)
+};
 
-    xhr.open('GET', postsData_url, true)
-
-    xhr.send(null)
+const fetchPosts = () => {
+    showLoader()
+    let xhr = makeAPIcall('GET', postsData_url)
 
     xhr.onload = () => {
         if(xhr.status >= 200 && xhr.status <= 299){
-            let data = JSON.parse(xhr.response)
+            let data = JSON.parse(xhr.response).reverse()
             templating(data)
         }else{
-            console.error(`Something went wrong...`)
+            let msg = `Something went wrong while fetching posts...`
+            console.error(msg)
+            snackBar(msg, `error`)
         }
+    }
+
+    xhr.onerror = () => {
+        snackBar(`Network error occurred during XHR request`, `error`)
     }
 };
 
 fetchPosts()
 
 const onPostAdd = (eve) => {
+    showLoader()
     eve.preventDefault()
 
     let newPost = {
@@ -102,13 +147,7 @@ const onPostAdd = (eve) => {
         body: bodyControl.value
     }
 
-    // POST method
-
-    let xhr = new XMLHttpRequest()
-
-    xhr.open('POST', postsData_url, true)
-
-    xhr.send(JSON.stringify(newPost))
+    let xhr = makeAPIcall('POST', postsData_url, newPost)
 
     xhr.onload = () => {
         if(xhr.status >= 200 && xhr.status <= 299){
@@ -116,35 +155,46 @@ const onPostAdd = (eve) => {
             localStorage.setItem('newId', newId)
             showNewPost(newPost)
         }else{
-            console.error(`Something went wrong...`)
+            let msg = `Something went wrong while submitting post...`
+            console.error(msg)
+            snackBar(msg, `error`)
         }
+    }
+
+    xhr.onerror = () => {
+        snackBar(`Network error occurred during XHR request`, `error`)
     }
 };
 
 const onEdit = (ele) => {
-    let editId = ele.closest('.col-md-4').id
+    showLoader()
+    let editId = ele.closest('.col-md-6').id
     localStorage.setItem('editId', editId)
+    let edit_url = `${postsData_url}/${editId}`
 
-    // GET method
-    let xhr = new XMLHttpRequest()
-
-    xhr.open('GET', `${postsData_url}/${editId}`, true)
-
-    xhr.send(null)
+    let xhr = makeAPIcall('GET', edit_url)
 
     xhr.onload = () => {
         if(xhr.status >= 200 && xhr.status <= 299){
             let editPost = JSON.parse(xhr.response)
             patch(editPost)
         }else{
-            console.error(`Something went wrong...`)
+            let msg = `Something went wrong while getting post...`
+            console.error(msg)
+            snackBar(msg, `error`)
         }
-    }    
+    } 
+    
+    xhr.onerror = () => {
+        snackBar(`Network error occurred during XHR request`, `error`)
+    }
 };
 
 const onPostUpdate = () => {
+    showLoader()
     let updateId = localStorage.getItem('editId')
     localStorage.removeItem('editId')
+    let update_url = `${postsData_url}/${updateId}`
 
     let updatedPost = {
         userId: userIdControl.value,
@@ -153,43 +203,58 @@ const onPostUpdate = () => {
         id: updateId
     }
 
-    // PUT method
-
-    let xhr = new XMLHttpRequest()
-
-    xhr.open('PATCH', `${postsData_url}/${updateId}`, true)
-
-    xhr.send(JSON.stringify(updatedPost))
+    let xhr = makeAPIcall('PATCH', update_url, updatedPost)
 
     xhr.onload = () => {
         if(xhr.status >= 200 && xhr.status <= 299){
-            // let newId = JSON.parse(xhr.response).id
-            // localStorage.setItem('newId', newId)
-            // showNewPost(newPost)
-            cl(xhr.response)
-            showUpdatedPost()
+            // let updatedPost = JSON.parse(xhr.response)
+            showUpdatedPost(updatedPost)
         }else{
-            console.error(`Something went wrong...`)
+            let msg = `Something went wrong while updating post...`
+            console.error(msg)
+            snackBar(msg, `error`)
         }
-    }    
+    }
+    
+    xhr.onerror = () => {
+        snackBar(`Network error occurred during XHR request`, `error`)
+    }
 };
 
+const onRemove = (ele) => {
+    let removeId = ele.closest('.col-md-6').id
+    let remove_url = `${postsData_url}/${removeId}`
 
+    Swal.fire({
+        title: "Do you really want to delete this post?",
+        text: "Post will be deleted permanently",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d9534f",
+        cancelButtonColor: "#0275d8",
+        confirmButtonText: "Delete"
+    }).then((result) => {
+        if (result.isConfirmed){
+            showLoader()
+            let xhr = makeAPIcall('DELETE', remove_url)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            xhr.onload = () => {
+                if(xhr.status >= 200 && xhr.status <= 299){
+                    ele.closest('.col-md-6').remove()
+                    snackBar(`Post deleted successfully!!!`, `success`)
+                }else{
+                    let msg = `Something went wrong while deleting post...`
+                    console.error(msg)
+                    snackBar(msg, `error`)
+                }
+            }
+            
+            xhr.onerror = () => {
+                snackBar(`Network error occurred during XHR request`, `error`)
+            }    
+        }
+});
+};
 
 postsForm.addEventListener('submit', onPostAdd);
 updateBtn.addEventListener('click', onPostUpdate);
